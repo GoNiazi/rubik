f5b1f60afa334161d1aa20f53af9b5c631f0fb8e
GONiazi <komailniazi@gmail.com>
2023-11-16 00:42:05 +0500

    working on homepage

---
+++ src/Components/HeroSection/HeroSection.js
@@ -0,0 +1,18 @@
+// import React from 'react'
+
+// const HeroSection = () => {
+//   return (
+//     <div className="">
+//         <div className="text-white mt-60">
+//         <div>
+//         </div>
+//         <div>
+//             <p className="text-white">شركة محلیة بخبرات
+// عالمیة we are Glocal</p>
+//         </div>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default HeroSection
---
+++ src/Pages/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import  "./Homepage.css"
+import HeroSection from "../../Components/HeroSection/HeroSection"
+const Homepage = () => {
+  return (
+    <div className="backgroundDiv">
+        {/* <HeroSection/> */}
+    </div>
+  )
+}
+
+export default Homepage
